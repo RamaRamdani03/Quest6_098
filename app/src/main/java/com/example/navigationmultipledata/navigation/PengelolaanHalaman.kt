6ee25884f0d618afbcb76589e87a2dc26836a920
RamaRamdani03 <ramaramdani449@gamil.com>
2024-11-22 19:50:13 +0700

    enum class Halaman untuk mengelola berbagai halaman aplikasi

---
+++ app/src/main/java/com/example/navigationmultipledata/navigation/PengelolaanHalaman.kt
@@ -0,0 +1,24 @@
+package com.example.navigationmultipledata.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationmultipledata.ui.screen.MahasiswaFormView
+import com.example.navigationmultipledata.ui.screen.RencanaStudyView
+import com.example.navigationmultipledata.ui.screen.SplashView
+import com.example.navigationmultipledata.ui.screen.TampilView
+import com.example.navigationmultipledata.ui.view.MahasiswaViewModel
+import com.example.navigationmultipledata.ui.view.RencanaStudyViewModel
+
+enum class Halaman{
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}

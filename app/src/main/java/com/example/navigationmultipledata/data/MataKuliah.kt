28d40be1cfbafbabe29424982c2ff11871302d20
RamaRamdani03 <ramaramdani449@gamil.com>
2024-11-22 19:10:03 +0700

    Tambahkan daftar mata kuliah ke dalam objek MataKuliah

---
+++ app/src/main/java/com/example/navigationmultipledata/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.navigationmultipledata.data
+
+object MataKuliah {
+    val options= listOf(
+        "Agama Islam dan kemuhammadiyahan",
+        "Pengembangan Aplikasi Web",
+        "Pancasila",
+        "Dasar Teknologi Informasi",
+        "Fisika dan Elektronika",
+        "Logika Teknik Pemograman",
+    )
+}

4ab2c0247f1c7df6dd1dbdc3c105a3c9e1d47b4f
RamaRamdani03 <ramaramdani449@gamil.com>
2024-11-22 19:17:58 +0700

    fungsi untuk mengubah dan menyimpan data KRS pada RencanaStudyViewModel

--- app/src/main/java/com/example/navigationmultipledata/ui/view/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigationmultipledata/ui/view/RencanaStudyViewModel.kt
@@ -10,4 +10,19 @@ import kotlinx.coroutines.flow.update
 class RencanaStudyViewModel:ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudy())
     val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow()
+
+    fun setMatakuliah(mkPilihan: String){
+        _krsState.update { stateMk -> stateMk.copy(matakuliah = mkPilihan) }
+    }
+
+    fun setKelas(KelasPilihan:String){
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = KelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>){
+        _krsState.update { status -> status.copy(
+            matakuliah = ls[0],
+            kelas = ls[1]
+        ) }
+    }
 }
